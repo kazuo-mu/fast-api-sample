d303858020884d0169fb926895ce92199eeb7616
Kazuo Murakami <wired2wireless4me@gmail.com>
2024-03-02 19:22:55 +0900

    add cruds

---
+++ app/api/cruds.py
@@ -0,0 +1,30 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from app.api.schemas import ProgrammerDetail
+from app.db.models import Programmer, ProgrammerDetail
+
+def get_programmers(
+  db: Session
+) -> list[Programmer]:
+  programmers = db.scalars(
+    select(
+      Programmer,
+    )
+    .order_by("id")
+  ).all()
+  return programmers
+
+def add_programmer(
+  db: Session,
+  programmer_detail: ProgrammerDetail,
+):
+  programmer = Programmer()
+  programmer.name = programmer_detail.name
+  programmer.twitter_id = programmer_detail.twitter_id
+  programmer.languages = [
+    ProgrammerLanguage(name=language)
+    for language in programmer_detail.languages
+  ]
+  db.add(programmer)
+  db.commit()
+  

74d8072468d82962adaa33c6bde5c887bc2e3e77
Kazuo Murakami <wired2wireless4me@gmail.com>
2024-03-02 19:39:32 +0900

    add a scheme validation

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class ProgrammerListItem(BaseModel):
   name: str
@@ -9,4 +9,6 @@ class ProgrammerListItem(BaseModel):
 class ProgrammerDetail(BaseModel):
   name: str
   twitter_id: str
-  languages: list[str]
+  languages: list[str] = Field(
+    ..., min_items=1, max_items=3
+  )
